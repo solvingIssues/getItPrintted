d2be8b932b728e142d4a3ce22287a245e928ad98
solvingIssues <carloscorreia@protonmail.com>
2023-05-19 13:16:32 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HeaderComponent from './components/header/header.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+  <main>
+    <TheWelcome />
+  </main>
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <header>
+        <div class="header">
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.header ul li{
+    display: inline-block;
+    padding: 5px;
+}
+
+
+@media (min-width: 1024px) {
+  
+}
+</style>
